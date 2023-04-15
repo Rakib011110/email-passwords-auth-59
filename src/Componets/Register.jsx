3abfc89de45ed5aadfb3419123439ab03f3e0ebf
Rakibul Islam <rakib011110@gmail.com>
2023-04-15 11:14:27 +0600

    from add

--- src/Componets/Register.jsx
+++ src/Componets/Register.jsx
@@ -3,7 +3,38 @@ import React from "react";
 const Register = () => {
   return (
     <div>
-      <h3> This is Register</h3>
+      <form>
+        <input
+          className="w-50 mb-4 rounded ps-2"
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Your Name"
+          required
+        />
+        <br />
+        <input
+          className="w-50 mb-4 rounded ps-2"
+          onChange={handleEmailChange}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Your Email"
+          required
+        />
+        <br />
+        <input
+          className="w-50 mb-4 rounded ps-2"
+          onBlur={handlePasswordBlur}
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Your Password"
+          required
+        />
+        <br />
+        <input className="btn btn-primary" type="submit" value="Register" />
+      </form>
     </div>
   );
 };
